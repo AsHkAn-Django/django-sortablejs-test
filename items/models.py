d66adf2b83ba7297582e579e9c7c141ed2cf4d4a
AsHkaN <ashkan_ahrari@yahoo.com>
2025-04-30 19:44:44 +0300

    first initial

---
+++ items/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.name
